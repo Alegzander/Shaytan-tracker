35483adc2dabd84df38bd8311667f651d977f616
Alegz <al@flatora.ru>
2012-08-31 11:52:54 +0500

    Started work on Yii version of tracker

---
+++ protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../../../Всякая всячина/Yii/yii-1.1.12.b600af/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
