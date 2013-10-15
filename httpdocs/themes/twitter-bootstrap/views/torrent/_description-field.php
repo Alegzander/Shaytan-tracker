8e663b39b306b9fcc24617ab12106de666cd476d
Alegzander <alexander.aka.alegz@gmail.com>
2013-10-15 19:53:45 +0500

    Commented ckeditor, turned textarea back. Will solve this problem later.

--- httpdocs/themes/twitter-bootstrap/views/torrent/_description-field.php
+++ httpdocs/themes/twitter-bootstrap/views/torrent/_description-field.php
@@ -14,6 +14,14 @@
 <?=$form->toggleButtonRow($formModel, $descriptionFromFileFieldName, array(
     'class' => 'description-from-file',
 ));?>
-<?=$form->textAreaRow($formModel, $descriptionFieldName, array(
-    'class' => 'description'
-));?>
+<?=$form->textAreaRow($formModel, $descriptionFieldName, array('class' => 'description'));?>
+<?//=$form->ckEditorRow($formModel, $descriptionFieldName, array(
+//    'class' => 'description',
+//    'options' => array(
+////        'plugins' => 'bbcode,image,dialog,toolbar,liststyle,image,imagepaste,clipboard,specialchar,stylesheetparser',
+//        'removePlugins' => 'iframe,iframedialog,font,flash,save,about,adobeair,forms,preview,specialchar,div,smiley,fakeobject',
+//        'resize_enabled' => 'js:false',
+////        'toolbar' => array('Source', '-', 'Bold', 'Italic')
+////        'startupMode' => 'source'
+//    )
+//));?>
