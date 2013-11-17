37949712642289746dda5398181e4509d152d3bb
Alegzander <alexander.aka.alegz@gmail.com>
2013-11-17 16:44:21 +0500

    Added indexes in torrent meta collection

--- protected/migrations/m131117_104630_add.php
+++ protected/migrations/m131117_104630_add.php
@@ -5,6 +5,7 @@ class m131117_104630_add extends CDbMigration
 	public function safeUp()
 	{
         TorrentMeta::model()->getCollection()->ensureIndex(array('torrentId' => 1), array('unique' => true));
+        TorrentMeta::model()->getCollection()->ensureIndex('name');
         TorrentMeta::model()->getCollection()->ensureIndex('suspend');
         TorrentMeta::model()->getCollection()->ensureIndex('hidden');
         TorrentMeta::model()->getCollection()->ensureIndex('size');
