b65cf32537eb72cc9ce081bd4d19b965adb4a964
Alegzander <alexander.aka.alegz@gmail.com>
2013-11-17 16:48:38 +0500

    Added more indexes.

---
+++ protected/migrations/m131117_114742_more_torrent_meta_indexes.php
@@ -0,0 +1,16 @@
+<?php
+
+class m131117_114742_more_torrent_meta_indexes extends CDbMigration
+{
+	public function safeUp()
+	{
+        TorrentMeta::model()->getCollection()->ensureIndex(array('_id' => -1), array('unique' => true));
+        TorrentMeta::model()->getCollection()->ensureIndex(array('hash' => 1), array('unique' => true));
+	}
+
+	public function down()
+	{
+		echo "m131117_114742_more_torrent_meta_indexes does not support migration down.\n";
+		return false;
+	}
+}
